a0ed57efb4e9d7fa67267aeed5eef1a1fcf5cc6f
Vadik <gloom16@mail.ru>
2024-02-11 20:18:30 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import com.google.gson.Gson;
+import model.Writer;
+
+public class Main {
+    public static void main(String[] args) {
+        Writer writer = new Writer(1, )
+        Gson gson = new Gson();
+    }
+}
--- src/main/java/controller/WriterController.java
+++ src/main/java/controller/WriterController.java
@@ -3,12 +3,33 @@ package controller;
 import model.Writer;
 import view.WriterView;
 
-import java.io.File;
+import java.util.List;
+import java.util.Optional;
 
 public class WriterController {
     private final WriterView writerView;
 
     public WriterController(WriterView writerService) {
         this.writerView = writerService;
+    }
+
+    public List<Writer> showAll() {
+        return writerView.findAll();
+    }
+
+    public Optional<Writer> showById(Long id) {
+        return writerView.findById(id);
+    }
+
+    public void deleteById(Long id) {
+        writerView.deleteById(id);
+    }
+
+    public void addWriter(Writer writer) {
+        writerView.addWriter(writer);
+    }
+
+    public void update(Long id, String s1, String s2) {
+        writerView.update(id, s1, s2);
     }
 }
